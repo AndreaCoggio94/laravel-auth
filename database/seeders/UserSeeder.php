6ef93e34581034d08d4d063bd63f42e308e289cd
AndreaCoggio94 <andreacoggio94@gmail.com>
2023-10-23 16:51:57 +0200

    trying to make the debug seeders

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,12 +2,13 @@
 
 namespace Database\Seeders;
 
-use\App\Models\User;
 
 use Illuminate\Support\Facades\Hash;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+
+use\App\Models\User;
 
 class UserSeeder extends Seeder
 {
